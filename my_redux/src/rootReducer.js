814c633859df26d20541c1acc0e3cfbde34d8075
Sunbridger <739272884@qq.com>
2018-12-12 22:28:12 +0800

    修改

--- my_redux/src/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import textRedux from './TextPage/reducer'
-import picRedux from './PicPage/reducer'
-
-const rooReducer=combineReducers({
-  textRedux,
-  picRedux
-});
-export default rooReducer;
