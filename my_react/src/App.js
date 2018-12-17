4890847ed63f2c2f42404c0927ae13c4b0b031ff
Sunbridger <739272884@qq.com>
2018-12-17 21:53:55 +0800

    'localstroage新手引导'

--- my_react/src/App.js
+++ my_react/src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+var localStorage = window.localStorage;
 class App extends Component {
+  state = {
+    tipForNewBox:localStorage.tipForNewBox
+  }
+  iknow(){
+    localStorage.setItem('tipForNewBox',true)
+    this.setState({tipForNewBox:localStorage.tipForNewBox})
+  }
   render(){
     return (
       <div>
-    asdkaksjdak  
+      {
+        !this.state.tipForNewBox&&(
+          <div className='div_box '>
+            <span className='iconsquare'></span>
+            <p>新的功能已经上线了</p>
+            <button onClick={this.iknow.bind(this)}>知道了</button>
+          </div>  
+        )
+      }
       </div>
     )
   }
